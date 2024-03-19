664b9071d5343d56c6e83fea465472b0772eeae4
Max Druzhinin <6406412+maxdzin@users.noreply.github.com>
2024-03-19 22:40:45 +0200

    chore: use eslintrc config

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,6 @@
   <div>
     <div class="container mx-auto max-w-screen-md px-4 sm:px-10">
       <WelcomeIntro />
-
-      <div>
-        <p>Some other page content...</p>
-      </div>
     </div>
   </div>
 </template>
